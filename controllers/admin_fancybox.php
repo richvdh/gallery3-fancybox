a9de56afeafaf84b480b61b738b4b6538e81ba77
Richard van der Hoff <git@rvanderhoff.org.uk>
2013-04-30 00:23:07 +0100

    add date to tile text in fancybox
    NB this depends on a change to the theme, to put the data in the
    right place on the thumbnail link.

--- controllers/admin_fancybox.php
+++ controllers/admin_fancybox.php
@@ -152,6 +152,7 @@ class Admin_fancybox_Controller extends Admin_Controller {
         if ($titlePosition != "hide"):
           $_title .= "'titleFormat' : function(title, currentArray, currentIndex, currentOpts) { ";
           $_titlestring = "'Image ' +  (currentIndex + 1) + ' / ' + currentArray.length + (title == '' ? '' : ': ' + title)";
+	  $_titlestring .= "+ ' (' + currentArray[currentIndex].getAttribute(\"data-date\") + ')'";
         endif;
         switch ($titlePosition):
           case "over":
